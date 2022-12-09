ffdf41a4f17a09facfe18bd341eb89939a0107f5
Quyết <congquyetgioan@gmail.com>
2022-12-10 00:46:48 +0700

    update log

--- connection.js
+++ connection.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect("mongodb+srv://phucne:ahihi123hihi@cluster0.mi4jjzj.mongodb.net/?retryWrites=true&w=majority", ()=> {
-  console.log('connected to mongodb')
+  console.log('connected to mongodb sucess!')
 })
